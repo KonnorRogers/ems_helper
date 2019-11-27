3cccf6552495aefd5eeb18a45a59bd0b810911bf
paramagicdev <konnor5456@gmail.com>
2019-11-27 11:59:53 -0500

    add eslint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(page_title: '')
+    base_title = 'EMS Helper'
+
+    return base_title if page_title.empty?
+
+    "#{page_title} | #{base_title}"
+  end
 end
