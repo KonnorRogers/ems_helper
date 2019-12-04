f95ddd847e21594ffb0550f0439cabefa77e0b95
paramagicdev <konnor5456@gmail.com>
2019-12-04 09:08:04 -0500

    remove nokogiri in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# Use nokogiri to scrape for the addresses for massachusetts
-require 'nokogiri'
-require 'open-uri'
-
-doc = Nokogiri::HTML(open('http://www.google.com/search?q=Bay State Medical Center'))  # Do funky things with it using Nokogiri::XML::Node methods...
-
-  ####
-  # Search for nodes by css
-  doc.css('h3.r a.l').each do |link|
-    puts link.content
-  end
-
-  ####
-  # Search for nodes by xpath
-  doc.xpath('//h3/a[@class="l"]').each do |link|
-    puts link.content
-  end
-
-  ####
-  # Or mix and match.
-  doc.search('h3.r a.l', '//h3/a[@class="l"]').each do |link|
-    puts link.content
-  end
