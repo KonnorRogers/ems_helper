6885db660e5c292b7540839425453d694b04dc1f
paramagicdev <konnor5456@gmail.com>
2019-12-04 08:01:17 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_195255) do
+ActiveRecord::Schema.define(version: 2019_12_03_204931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_195255) do
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["latitude", "longitude"], name: "index_hospitals_on_latitude_and_longitude"
   end
 
 end
