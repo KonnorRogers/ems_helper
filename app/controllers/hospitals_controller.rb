e3d65dee7aa6d9d5346ed4ab65ad302e4c70a126
paramagicdev <konnor5456@gmail.com>
2019-12-04 22:24:48 -0500

    add hospital routes, add seeds.rb

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,2 +1,25 @@
 class HospitalsController < ApplicationController
+  # GET /hospitals
+  # display all hospitals
+  def index
+    @hospitals = Hospital.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
