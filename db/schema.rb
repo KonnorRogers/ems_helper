68c9a2ce0a9941ece431893bdcf6482261b54007
paramagicdev <konnor5456@gmail.com>
2019-12-05 06:57:33 -0500

    add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_204931) do
+ActiveRecord::Schema.define(version: 2019_12_05_115558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2019_12_03_204931) do
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone_number"
+    t.integer "trauma_level"
+    t.string "website"
+    t.string "name"
+    t.integer "zip"
     t.index ["latitude", "longitude"], name: "index_hospitals_on_latitude_and_longitude"
   end
 
