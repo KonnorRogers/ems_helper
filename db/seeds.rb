c6a240b9f7e57157e11ca9369abb81b20215da74
paramagicdev <konnor5456@gmail.com>
2019-12-09 03:27:45 -0500

    remove stuff from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,39 +12,3 @@
 # https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/Hospitals_1/FeatureServer/0/query?outFields=*&where=1%3D1
 # see above for seeding the db
 
-require 'net/http'
-require 'json'
-
-url = 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/Hospitals_1/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
-uri = URI(url)
-
-response = Net::HTTP.get(uri)
-json_response = JSON.parse(response)
-
-FIELDS = %w[STATE CITY WEBSITE NAME TELEPHONE TRAUMA LONGITUDE LATITUDE ZIP ADDRESS COUNTRY].freeze
-array_of_hospitals = json_response['features']
-
-# Trauma levels are only 1-3
-def trauma_level(string)
-  string.split('LEVEL ').join('').length
-end
-
-array_of_hospitals.each do |hospital|
-  hospital['attributes'].each do |k, _v|
-    next unless FIELDS.include?(k)
-
-    Hospital.create(
-      name: k['NAME'],
-      street: k['ADDRESS'],
-      state: k['STATE'],
-      city: k['CITY'],
-      zip_code: k['ZIP'],
-      country: k['COUNTRY'],
-      website: k['WEBSITE'],
-      telephone: k['TELEPHONE'],
-      trauma_level: k['TRAUMA'],
-      longitude: k['LONGITUDE'],
-      latitude: k['LATITUDE']
-    )
-  end
-end
